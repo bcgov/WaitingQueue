1ef752c9fa289dcd492054e42daf50e5c28af903
ikethecoder <ikethecoder@copeconsulting.ca>
2023-02-10 16:29:24 -0800

    adjust interval time

--- Apps/Frontend/src/queue-poller.js
+++ Apps/Frontend/src/queue-poller.js
@@ -140,7 +140,7 @@ class QueuePoller extends HTMLElement {
             localStorage.setItem(STORAGE_KEY, JSON.stringify(json));
             this.#interval = setInterval(
                 this.#refreshTicket,
-                json.checkInAfter - new Date().getTime() + 1000
+                json.checkInAfter * 1000 - new Date().getTime() + 1000
             );
             this.#updatePosition();
         } catch (err) {
