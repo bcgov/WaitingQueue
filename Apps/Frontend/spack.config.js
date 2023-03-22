dcbbca6ba7161b5a1fc4cb392f19e5617ea64500
Joshua Jones <joshua@general-metrics.com>
2023-03-22 12:32:14 -0700

    Separate builds, minify css

--- Apps/Frontend/spack.config.js
+++ Apps/Frontend/spack.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   mode: "production",
   entry: {
-    main: __dirname + "/src/main.js",
-    ie: __dirname + "/src/ie.js",
+    legacy: __dirname + "/src/ie.js",
   },
   output: {
     path: __dirname + "/dist",
