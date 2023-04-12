ea7685203e15bddb694adb15c4105fb6053cf1f0
Joshua Jones <joshua@general-metrics.com>
2023-04-12 14:33:23 -0700

    POC language support

---
+++ Apps/Frontend/src/lang.js
@@ -0,0 +1,40 @@
+/**
+ * Checks to see if any format of the user's preferred language is supported by the application
+ *
+ * @param {string} preferredLanguage - The user's preferred language, should be taken from `navigator.language`
+ * @param {string[]} supportedLanguages - Languages supported from configuration
+ * @returns string | undefined - The supported, preferred language
+ */
+export function findUserLanguage(preferredLanguage, supportedLanguages) {
+  const preferred = preferredLanguage.toLowerCase();
+  const languageSupported = supportedLanguages.find((l) => {
+    // strict match, language or <language>-<region>
+    if (preferred === l.toLowerCase()) {
+      return true;
+    }
+
+    // language only
+    const preferredLanguageOnly = preferredLanguage.split("-")[0];
+    const supportedLanguagesOnly = l.split("-")[0];
+    if (supportedLanguagesOnly === preferredLanguageOnly) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return languageSupported;
+}
+
+export function checkLanguage() {
+  const userLanguage = navigator.language;
+  /** @type string[] */
+  const supportedLanguages = globalThis.supportedLanguages;
+  const language = findUserLanguage(userLanguage, supportedLanguages);
+  const currentLanguage = document.querySelector("html")?.getAttribute("lang");
+
+  if (language && currentLanguage !== language) {
+    const languageRedirectUrl = new URL(`/${language}`, location.href);
+    location.assign(languageRedirectUrl);
+  }
+}
--- Apps/Frontend/src/main.js
+++ Apps/Frontend/src/main.js
@@ -1,4 +1,7 @@
 import "./loading-spinner.js";
 import "./queue-poller.js";
+import { checkLanguage } from "./lang.js";
 
 export { handleTokenRefresh } from "./request.js";
+
+checkLanguage();
