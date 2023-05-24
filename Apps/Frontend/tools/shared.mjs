26184b713aaf1cd94a3ede5175b3e27bce43eb28
Joshua Jones <joshua@general-metrics.com>
2023-05-24 15:04:14 -0700

    Add supported languages utils file

---
+++ Apps/Frontend/tools/shared.mjs
@@ -0,0 +1,18 @@
+/** @type Record<string, string> */
+export const languages = {
+  "en-CA": "English",
+  "fr-CA": "Français",
+  "pa-IN": "ਪੰਜਾਬੀ",
+  "zh-CN": "中文 (中国)",
+  "zh-TW": "中文 (台灣)",
+  "ko-KR": "한국어",
+  tl: "Tagalog",
+  "fa-IR": "فارسی",
+  "es-ES": "Español",
+  vi: "Tiếng Việt",
+  "hi-IN": "हिन्दी",
+  ar: "العربية",
+  ja: "日本語",
+  "pt-PT": "Português",
+  ur: "اردو",
+};
